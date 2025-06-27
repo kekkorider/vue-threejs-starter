8feafd37f2bdf0d77414ac87212be7e4d2f8019a
Francesco Michelini <hello@francescomichelini.com>
2025-06-27 21:44:27 +0200

    Update SampleTSLMaterial

--- src/assets/materials/SampleTSLMaterial.js
+++ src/assets/materials/SampleTSLMaterial.js
@@ -1,4 +1,4 @@
-import { uv, Fn, vec4, positionLocal, uniform } from 'three/tsl'
+import { transformedNormalView, Fn, vec4, positionLocal, uniform } from 'three/tsl'
 import { MeshBasicNodeMaterial } from 'three/webgpu'
 
 export const SampleTSLMaterial = new MeshBasicNodeMaterial()
@@ -10,5 +10,5 @@ SampleTSLMaterial.positionNode = Fn(() => {
 })()
 
 SampleTSLMaterial.colorNode = Fn(() => {
-  return vec4(uv(), 0.5, 1.0)
+  return vec4(transformedNormalView, 1.0)
 })()

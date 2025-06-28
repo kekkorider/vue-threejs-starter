9d10ea1fe1c430db5f9e1757b209942577eb1543
Francesco Michelini <hello@francescomichelini.com>
2025-06-28 15:27:27 +0200

    Add GLSL plugin

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    glsl(),
     topLevelAwait({
       // The export name of top-level await promise for each chunk module
       promiseExportName: "__tla",
